ffecc94a5130407556d5d8f61071c4ed665d91e2
nacerkraa <nacer.kraa@univ-constantine2.dz>
2024-04-08 16:06:04 +0000

    Update about component, and add skills component

--- src/components/about/Info.jsx
+++ src/components/about/Info.jsx
@@ -2,7 +2,25 @@ import React from 'react'
 
 const Info = () => {
   return (
-    <div>Info</div>
+    <div className="about__info grid">
+      <div className="about__box">
+        <i className="bx bx-award about__icon"></i>
+        <h3 className="about__title">Experience</h3>
+        <span className="about__subtitle">6 months of working</span>
+      </div>
+
+      <div className="about__box">
+        <i className="bx bx-briefcase-alt about__icon"></i>
+        <h3 className="about__title">Complited</h3>
+        <span className="about__subtitle">10+ Projects</span>
+      </div>
+
+      <div className="about__box">
+        <i className="bx bx-support about__icon"></i>
+        <h3 className="about__title">Support</h3>
+        <span className="about__subtitle">Online 24/7</span>
+      </div>
+    </div>
   )
 }
 
