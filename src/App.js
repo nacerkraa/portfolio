76c47cb3fa756d684d1d730cbed3ceb415211a82
nacer kraa <79715038+nacerkraa@users.noreply.github.com>
2026-09-10 12:44:01 +0200

    Add core portfolio sections

--- src/App.js
+++ src/App.js
@@ -1,17 +1,25 @@
 import './App.css';
 import About from './components/about/About';
+import Contact from './components/contact/Contact';
+import Experience from './components/experience/Experience';
 import Header from './components/header/Header';
 import Home from './components/home/Home';
+import Projects from './components/projects/Projects';
+import Services from './components/services/Services';
 import Skills from './components/skills/Skills';
 
 function App() {
   return (
     <>
       <Header />
-      <main className='main'>
+      <main className="main">
         <Home />
         <About />
         <Skills />
+        <Experience />
+        <Projects />
+        <Services />
+        <Contact />
       </main>
     </>
   );
